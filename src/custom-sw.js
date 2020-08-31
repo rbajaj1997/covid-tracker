dd8740d59ba1797ddb08bc244faf24e09ffa6775
Rohit Bajaj <rbajaj1402@gmail.com>
2020-08-31 10:24:50 +0530

    PWA final touches

--- src/custom-sw.js
+++ src/custom-sw.js
@@ -10,7 +10,10 @@ if (workbox) {
 
 
 self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('activate', event => {
+    console.log('Activated!');
+    return event.waitUntil(self.clients.claim())
+});
 
 // We need this in Webpack plugin (refer to swSrc option): https://developers.google.com/web/tools/workbox/modules/workbox-webpack-plugin#full_injectmanifest_config
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
